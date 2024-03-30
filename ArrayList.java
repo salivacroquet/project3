919c2f2fbb68a7dc719356bc64c17668f45ae3c6
salivacroquet <135278040+salivacroquet@users.noreply.github.com>
2024-03-29 19:57:21 -0500

    added required methods

--- ArrayList.java
+++ ArrayList.java
@@ -1,1 +1,75 @@
-//Project 3
+
+
+public class ArrayList<T extends Comparable<T>> implements List<T> {
+
+    public ArrayList() {
+
+
+    }
+
+
+    boolean add(T element) {
+
+    }
+
+    boolean add(int index, T element) {
+
+    }
+
+    void clear() {
+
+    }
+
+    T get(int index) {
+
+    }
+
+    int indexOf(T element) {
+
+    }
+
+    boolean isEmpty() {
+
+    }
+
+    int size() {
+
+    }
+
+    void sort() {
+
+    }
+
+    T remove(int index) {
+
+    }
+
+    void removeDuplicates() {
+
+    }
+
+    void reverse() {
+
+    }
+
+    void exclusiveOr(List<T> otherList) {
+
+    }
+
+    T getMin() {
+
+    }
+
+    T getMax() {
+
+    }
+
+    String toString() {
+
+    }
+
+    boolean isSorted() {
+
+    }
+
+}
---
+++ LinkedList.java
@@ -0,0 +1,76 @@
+
+public class LinkedList<T extends Comparable<T>> implements List<T> {
+
+
+
+    public LinkedList() {
+
+
+    }
+
+    boolean add(T element){
+
+    }
+
+    boolean add(int index, T element){
+
+    }
+
+    void clear(){
+
+    }
+
+    T get(int index){
+
+    }
+
+    int indexOf(T element){
+
+    }
+
+    boolean isEmpty(){
+
+    }
+
+    int size(){
+
+    }
+
+    void sort(){
+
+    }
+
+    T remove(int index){
+
+    }
+
+    void removeDuplicates(){
+
+    }
+
+    void reverse(){
+
+    }
+
+    void exclusiveOr(List<T> otherList){
+
+    }
+
+    T getMin(){
+
+    }
+
+    T getMax(){
+
+    }
+
+    String toString(){
+
+    }
+
+    boolean isSorted(){
+
+    }
+
+}
+
