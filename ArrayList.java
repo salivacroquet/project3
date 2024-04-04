8981534d1144c8f76d1c537bb809b06f429e9a7b
salivacroquet <135278040+salivacroquet@users.noreply.github.com>
2024-04-03 19:36:03 -0500

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -85,7 +85,15 @@ public class ArrayList<T extends Comparable<T>> implements List<T> {
     }
 
     public void sort() { //SYLVIA
-
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1 ] = temp;
+                }
+            }
+        }
     }
 
     public T remove(int index) { //HAJAR
